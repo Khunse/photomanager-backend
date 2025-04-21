a8c2d0ca415061252c6a28bb91ae96857f94ff55
Khunse <louis.escar.k@gmail.com>
2025-04-21 20:28:24 +0630

    - [Modify] remove migration files

--- init.sql
+++ init.sql
@@ -1,5 +1,6 @@
 create user myuser with password 'Aungaung123!';
 create database "imageuploadmanagementsystem.dev";
+\c "imageuploadmanagementsystem.dev";
+grant usage, create on schema public to myuser;
 grant all privileges on database "imageuploadmanagementsystem.dev" to myuser;
-grant all privileges on all tables in schema public to myuser;
-alter default privileges in schema public grant all privileges on tables to myuser;
+alter default privileges in schema public grant select,insert, update,delete on tables to myuser;
--- setup.sh
+++ setup.sh
@@ -11,30 +11,31 @@ $HOME/dotnet-9-sdk/dotnet tool install -g dotnet-ef
 
 export DOTNET_ROOT=$HOME/dotnet-9-sdk
 export PATH=$PATH:$HOME/dotnet-9-sdk
+export PATH=$PATH:$HOME/.dotnet/tools
 
-# sudo apt-get install -y nginx
+sudo apt-get install -y nginx
 sudo apt-get install -y postgresql postgresql-contrib
-# sudo apt-get install -y certbot
-# sudo apt-get install -y python3-nginx-certbot 
+sudo apt-get install -y certbot
+sudo apt-get install -y python3-nginx-certbot 
 
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
 sudo -u postgres psql -f /tmp/my_init.sql
-# sudo certbot --nginx -d api.photomanager.site
+sudo certbot --nginx -d api.photomanager.site
 
 sudo git clone https://github.com/Khunse/photomanager-backend.git
-# cd $HOME/photomanager-backend
-# dotnet publish -c Release -o ./publish
+sudo chow ubunt:ubuntu -R $HOME/photomanager-backend
+cd $HOME/photomanager-backend
+dotnet ef migrations add 'create table'
+dotnet ef database update
+dotnet publish -c Release -o ./publish
 
-# sudo mv /tmp/myfile/myapp.service /etc/systemd/system/
-# sudo mv /tmp/myfile/myapp.conf /etc/nignx/conf.d/
+sudo mv /tmp/myapp.service /etc/systemd/system/
+sudo mv /tmp/myapp.conf /etc/nignx/conf.d/
 
-# sudo systemctl start myapp.service
-# sudo systemctl enable myapp.service
+sudo systemctl start myapp.service
+sudo systemctl enable myapp.service
 
-# sudo systemctl start nginx
-# sudo systemctl enable nginx
-
-
-# sudo rm -rf /tmp/myfile
+sudo systemctl start nginx
+sudo systemctl enable nginx
